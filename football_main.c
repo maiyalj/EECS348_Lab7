91cc8f36fc3be0eae72242aece344b138287a326
maiyalj <mlauralj7@gmail.com>
2025-04-03 01:45:39 -0500

    Update football_main.c

--- football_main.c
+++ football_main.c
@@ -1,5 +1,22 @@
+#include <stdio.h>
 #include "football.h"
 
-int main(int argc, char *argv[]) {
+// main function
+int main() {
+    // declare integer variable score 
+    int score;
+    //while true, run program
+    while (1) {
+        // ask user to enter value for the score
+        printf("\nEnter 0 or 1 to STOP\n");
+        printf("Enter the NFL score: ");
+        scanf("%d", &score);
+        // end program if user enters 0 or 1 
+        if (score <= 1) {
+            break;
+        }
+        // run print_combinations function
+        print_combinations(score);
+    }
     return 0;
 }
